c8a19c47a4a01514e3cb6be3691fd921b0ed7d73
dzhankhaev <dzhankhaev.d@gmail.com>
2020-03-15 08:16:04 +0300

    circus

--- src/fractol.c
+++ src/fractol.c
@@ -16,7 +16,7 @@
 
 static void	init(t_fr *fr)
 {
-	fr->width = 1920;
+	fr->width = 1080;
 	fr->height = 1080;
 	fr->mlx = mlx_init();
 	fr->win = mlx_new_window(fr->mlx, fr->width, fr->height, "fractol");
@@ -31,7 +31,6 @@ static void	init(t_fr *fr)
 			* fr->height / fr->width;
 	fr->ms = create_cmplx((fr->max.re - fr->min.re) / (fr->width - 1),
 			(fr->max.im - fr->min.im) / (fr->height - 1));
-	fr->max.im = (-1 * fr->min.im + fr->max.im) / 2;
 }
 
 void		cross(t_fr *fr)
@@ -61,6 +60,36 @@ void		cross(t_fr *fr)
 	mlx_put_image_to_window(fr->mlx, fr->win, fr->img, 0, 0);
 }
 
+void		circus(t_fr *fr)
+{
+	char	*temp;
+	t_point	c;
+	t_point	z;
+	int		x;
+	int		y;
+	y = 0;
+	while (y < fr->height)
+	{
+		c.im = fr->max.im - y * fr->ms.im;
+		x = 0;
+		while (x < fr->width)
+		{
+			c.re = fr->min.re + x * fr->ms.re;
+			z = create_cmplx(c.re, c.im);
+			if (pow(z.re, 2) + pow(z.im, 2) <= 4)
+			{
+				temp = (char *)(fr->line + (y * 4 * fr->width));
+				temp[x * 4] = (char)25;
+				temp[x * 4 + 1] = (char)0;
+				temp[x * 4 + 2] = (char)0;
+			}
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(fr->mlx, fr->win, fr->img, 0, 0);
+}
+
 int			main(void)
 {
 	t_fr	fr;
@@ -72,6 +101,7 @@ int			main(void)
 	int		iter;
 
 	init(&fr);
+	circus(&fr);
 	y = 0;
 	while (y < fr.height)
 	{
@@ -82,9 +112,9 @@ int			main(void)
 			c.re = fr.min.re + x * fr.ms.re;
 			z = create_cmplx(c.re, c.im);
 			iter = 0;
-			while ((z.re * z.re + z.im + z.im) <= 4 && iter < 50)
+			while (pow(z.re, 2) + pow(z.im, 2) <= 4 && iter < 50)
 			{
-				z = create_cmplx(z.re * z.re - z.im * z.im + c.re,
+				z = create_cmplx(pow(z.re, 2) - pow(z.im, 2) + c.re,
 						2.0 * z.re * z.im + c.im);
 				iter++;
 			}
@@ -100,9 +130,38 @@ int			main(void)
 		y++;
 	}
 
+/*
+	while (y < fr.height)
+	{
+		c.im = fr.max.im - y * fr.ms.im;
+		x = 0;
+		while (x < fr.width)
+		{
+			c.re = fr.min.re + x * fr.ms.re;
+			z = create_cmplx(c.re, c.im);
+			iter = 0;
+			while (pow(z.re, 2) + pow(z.im, 2) <= 4
+				   && iter < 50)
+			{
+				z = create_cmplx(
+						pow(z.re, 2) - pow(z.im, 2) + c.re,
+						-2 * fabs(z.re * z.im) + c.im);
+				iter++;
+			}
+			if (iter == 50)
+			{
+				temp = (char *)(fr.line + (y * 4 * fr.width));
+				temp[x * 4] = (char)255;
+				temp[x * 4 + 1] = (char)255;
+				temp[x * 4 + 2] = (char)255;
+			}
+			x++;
+		}
+		y++;
+	}*/
+
 	mlx_put_image_to_window(fr.mlx, fr.win, fr.img, 0, 0);
 
-//	cross(&fr);
 
 	mlx_hook(fr.win, 17, 0, red_cross, (void *)&fr);
 	mlx_key_hook(fr.win, key_hooks, (void *)&fr);
